4cca57e97c855b16d7bd4cd05289af45e3fff2d8
RIFAT RAHVALI <rifatrahvali@outlook.com>
2024-03-14 16:36:52 +0300

    Dinamik ve Render Dinamik içerikle çalışmak ve şablonları kullanarak templates dizini altındaki HTML sayfaları oluşturmak veya erişim sağlamak için kullanılır.

--- tcore/urls.py
+++ tcore/urls.py
@@ -1,6 +1,7 @@
 # myapp/urls.py
 from django.urls import path
-from .views import index
+from .views import *
 urlpatterns = [
 	path('', index, name='index'),
+    path('welcome/', welcome_user, name='welcome_user'),
 ]
--- tcore/views.py
+++ tcore/views.py
@@ -18,3 +18,10 @@ def merhaba(request):
     result = "Merhaba, Django!" 
 	# HttpResponse ile yanıt döndürme
     return HttpResponse(result)
+
+
+def welcome_user(request):
+    # Kullanıcı adını simüle etmek için bir bağlam (context) oluşturuyoruz
+    user_name = "Rıfat Rahvalı"
+    # render fonksiyonu ile HTML şablonunu kullanarak bir HTTP yanıtı oluşturuyoruz
+    return render(request, 'welcome_user.html', {'user_name': user_name})
